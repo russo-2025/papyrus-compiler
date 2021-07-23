ab569cb26a94b078d059d363118f0c66b6f840e8
russo-2025 <russo-2025@mail.ru>
2021-07-23 22:41:05 +0300

    Revert "add showcc flag"
    This reverts commit 9f8fc276a6adeaf4e0dc0aabc0160646073a1754.

--- build compiler.bat
+++ build compiler.bat
@@ -1,2 +1,2 @@
-v -g -cc msvc -m64 -os windows -showcc -o "bin\papyrus.exe" -path "@vlib|@vmodules|modules" "compiler"
+v -g -cc msvc -m64 -os windows -o "bin\papyrus.exe" -path "@vlib|@vmodules|modules" "compiler"
 pause
