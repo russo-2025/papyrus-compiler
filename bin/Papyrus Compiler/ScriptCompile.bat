e569ea3fc9b226ce0ac0cf5536e78dd617b5438a
russo-2025 <russo-2025@mail.ru>
2021-07-25 19:30:08 +0300

    add original compiler files

---
+++ bin/Papyrus Compiler/ScriptCompile.bat
@@ -0,0 +1,3 @@
+set mainPath=D:\SteamLibrary\steamapps\common\Skyrim Special Edition\
+"%mainPath%PapyrusCompiler.exe" %1 -f="%mainPath%TESV_Papyrus_Flags.flg" -i="%mainPath%Data\Source\Scripts" -o="%mainPath%Data\Scripts\Source"
+pause
