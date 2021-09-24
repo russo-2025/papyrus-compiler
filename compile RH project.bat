7b66b7b615cce5a29d2ae51791bfbbcafe1ce923
russo-2025 <russo-2025@mail.ru>
2021-09-24 22:30:17 +0300

    cleanup

--- compile RH project.bat
+++
@@ -1,3 +0,0 @@
-cd bin
-papyrus -compile -nocache -crutches -i "D:\_projects\RH-workspace\scripts\GM" "D:\_projects\RH-workspace\scripts\Custom" "D:\_projects\RH-workspace\scripts\Extension" -o "../test-files/compiled"
-pause
