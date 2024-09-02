5092cfbf784546dc929bbc88c6c417c4f91f0eef
russo-2025 <russo-2025@mail.ru>
2024-09-02 20:54:24 +0300

    wip

---
+++ modules/tests/selective_headers_loading_test.v
@@ -0,0 +1,11 @@
+module tests
+/*
+"Scriptname TestSelectiveLoading
+
+Float Function main(Float value, ObjectReference ref)
+    ref.GetDisplayName() ; ObjectReference
+    ref.GetFormID() ; ObjectReference
+    Game.GetPlayer()
+  Return value
+EndFunction"
+*/
