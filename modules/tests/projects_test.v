4d8d61018590d1d253e6e2da1cd0f1cf9f21b5b0
russo-2025 <russo-2025@mail.ru>
2025-12-24 19:43:42 +0300

    Refactor test project source path usage
    Introduced constants for source file paths to reduce repetition and improve readability in test functions. All test functions now use these constants instead of repeated get_source_dir calls.

--- modules/tests/projects_test.v
+++ modules/tests/projects_test.v
@@ -41,40 +41,63 @@ fn get_output_dir(dir_name string) string {
 	return path
 }
 
+const skyrim_deps = get_source_dir("psc_deps", "Form.psc")
+const skyui_sdk_51 = get_source_dir("SkyuiSDKSources_v5.1", "SKI_ConfigBase.psc")
+const skyui_sdk_52 = get_source_dir("SkyuiSDKSources_v5.2", "SKI_ConfigBase.psc")
+const mcm_helper = get_source_dir("MCMHelperSources", "MCM_ConfigBase.psc")
+const lovense_api = get_source_dir("LovenseAPISources", "Lovense.psc")
+const fnis = get_source_dir("FNISSources", "fnis.psc")
+const mfgfix = get_source_dir("MfgFixSources", "MfgFix_Settings.psc")
+const libfire = get_source_dir("LibFireSources", "LibFire.psc")
+const libturtleclub = get_source_dir("LibTurtleClubSources", "LibTurtleClub.psc")
+const papyrus_util = get_source_dir("PapyrusUtilSources", "PapyrusUtil.psc")
+const console_util = get_source_dir("ConsoleUtilSources", "ConsoleUtil.psc")
+const ni_override = get_source_dir("NiOverrideSources", "NiOverride.psc")
+const race_menu = get_source_dir("RaceMenuSources", "racemenu.psc")
+const ui_extensions = get_source_dir("UIExtensionsSources", "UIExtensions.psc")
+const lib_mathf = get_source_dir("LibMathfSources", "LibMathf.psc")
+const jcontainers = get_source_dir("JContainersSources", "JContainers.psc")
+const iequip = get_source_dir("iEquipSources", "dubhMonitorEffectScript.psc")
+const mantella_spell = get_source_dir("MantellaSpellSources", "MantellaLauncher.psc")
+const master_of_disguise = get_source_dir("MasterOfDisguiseSources", "dubhDisguiseMCMQuestScript.psc")
+const osa = get_source_dir("OSASources", "OSA.psc")
+const ostim = get_source_dir("OStimNGSources", "OStimAddon.psc")
+const sexlab = get_source_dir("SexLabSources", "SexLabFramework.psc")
+
 fn test_project_skyrim_deps_sources() {
-	prefs := get_prefs(get_source_dir("psc_deps", "Form.psc"), [], get_output_dir("LibFire"))
+	prefs := get_prefs(skyrim_deps, [], get_output_dir("LibFire"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_skyui_sdk_51() {
-	prefs := get_prefs(get_source_dir("SkyuiSDKSources_v5.1", "SKI_ConfigBase.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(skyui_sdk_51, [
+		skyrim_deps
 	], get_output_dir("SkyuiSDK"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_skyui_sdk_52() {
-	prefs := get_prefs(get_source_dir("SkyuiSDKSources_v5.2", "SKI_ConfigBase.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(skyui_sdk_52, [
+		skyrim_deps
 	], get_output_dir("SkyuiSDK"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_mcm_helper() {
-	prefs := get_prefs(get_source_dir("MCMHelperSources", "MCM_ConfigBase.psc"), [
-		get_source_dir("psc_deps", "Form.psc"),
-		get_source_dir("SkyuiSDKSources_v5.2", "SKI_ConfigBase.psc")
+	prefs := get_prefs(mcm_helper, [
+		skyrim_deps,
+		skyui_sdk_52
 	], get_output_dir("MCMHelper"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_lovense_api() {
-	prefs := get_prefs(get_source_dir("LovenseAPISources", "Lovense.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(lovense_api, [
+		skyrim_deps
 	], get_output_dir("LovenseAPI"))
 
 	builder.compile(&prefs)
@@ -82,8 +105,8 @@ fn test_project_lovense_api() {
 
 fn test_project_fnis()
 {
-	prefs := get_prefs(get_source_dir("FNISSources", "fnis.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(fnis, [
+		skyrim_deps
 	], get_output_dir("FNIS"))
 
 	builder.compile(&prefs)
@@ -91,155 +114,155 @@ fn test_project_fnis()
 
 fn test_project_mfgfix()
 {
-	prefs := get_prefs(get_source_dir("MfgFixSources", "MfgFix_Settings.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(mfgfix, [
+		skyrim_deps
 	], get_output_dir("MfgFix"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_libfire() {
-	prefs := get_prefs(get_source_dir("LibFireSources", "LibFire.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(libfire, [
+		skyrim_deps
 	], get_output_dir("LibFire"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_libturtleclub() {
-	prefs := get_prefs(get_source_dir("LibTurtleClubSources", "LibTurtleClub.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(libturtleclub, [
+		skyrim_deps
 	], get_output_dir("LibTurtleClub"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_papyrus_util() {
-	prefs := get_prefs(get_source_dir("PapyrusUtilSources", "PapyrusUtil.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(papyrus_util, [
+		skyrim_deps
 	], get_output_dir("PapyrusUtil"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_console_util() {
-	prefs := get_prefs(get_source_dir("ConsoleUtilSources", "ConsoleUtil.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(console_util, [
+		skyrim_deps
 	], get_output_dir("ConsoleUtil"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_ni_override() {
-	prefs := get_prefs(get_source_dir("NiOverrideSources", "NiOverride.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(ni_override, [
+		skyrim_deps
 	], get_output_dir("NiOverride"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_race_menu() {
-	prefs := get_prefs(get_source_dir("RaceMenuSources", "racemenu.psc"), [
-		get_source_dir("psc_deps", "Form.psc"),
-		get_source_dir("NiOverrideSources", "NiOverride.psc")
+	prefs := get_prefs(race_menu, [
+		skyrim_deps,
+		ni_override
 	], get_output_dir("RaceMenu"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_ui_extensions() {
-	prefs := get_prefs(get_source_dir("UIExtensionsSources", "UIExtensions.psc"), [
-		get_source_dir("psc_deps", "Form.psc"),
-		get_source_dir("NiOverrideSources", "NiOverride.psc"),
-		get_source_dir("RaceMenuSources", "racemenu.psc")
+	prefs := get_prefs(ui_extensions, [
+		skyrim_deps,
+		ni_override,
+		race_menu
 	], get_output_dir("UIExtensions"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_lib_mathf() {
-	prefs := get_prefs(get_source_dir("LibMathfSources", "LibMathf.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(lib_mathf, [
+		skyrim_deps
 	], get_output_dir("LibMathf"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_jcontainers() {
-	prefs := get_prefs(get_source_dir("JContainersSources", "JContainers.psc"), [
-		get_source_dir("psc_deps", "Form.psc")
+	prefs := get_prefs(jcontainers, [
+		skyrim_deps
 	], get_output_dir("JContainers"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_iequip() {
-	prefs := get_prefs(get_source_dir("iEquipSources", "dubhMonitorEffectScript.psc"), [
-		get_source_dir("psc_deps", "Form.psc"),
-		get_source_dir("SkyuiSDKSources_v5.2", "SKI_ConfigBase.psc"),
-		get_source_dir("LibTurtleClubSources", "LibTurtleClub.psc"),
-		get_source_dir("LibMathfSources", "LibMathf.psc"),
-		get_source_dir("LibFireSources", "LibFire.psc")
+	prefs := get_prefs(iequip, [
+		skyrim_deps,
+		skyui_sdk_52,
+		libturtleclub,
+		lib_mathf,
+		libfire
 	], get_output_dir("iEquip"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_mantella_spell() {
-	prefs := get_prefs(get_source_dir("MantellaSpellSources", "MantellaLauncher.psc"), [
-		get_source_dir("psc_deps", "Form.psc"),
-		get_source_dir("UIExtensionsSources", "UIExtensions.psc"),
-		get_source_dir("SkyuiSDKSources_v5.1", "SKI_ConfigBase.psc")
+	prefs := get_prefs(mantella_spell, [
+		skyrim_deps,
+		ui_extensions,
+		skyui_sdk_51
 	], get_output_dir("MantellaSpell"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_master_of_disguise() {
-	prefs := get_prefs(get_source_dir("MasterOfDisguiseSources", "dubhDisguiseMCMQuestScript.psc"), [
-		get_source_dir("psc_deps", "Form.psc"),
-		get_source_dir("LibFireSources", "LibFire.psc"),
-		get_source_dir("LibTurtleClubSources", "LibTurtleClub.psc"),
-		get_source_dir("SkyuiSDKSources_v5.1", "SKI_ConfigBase.psc"),
-		get_source_dir("LibMathfSources", "LibMathf.psc")
+	prefs := get_prefs(master_of_disguise, [
+		skyrim_deps,
+		libfire,
+		libturtleclub,
+		skyui_sdk_51,
+		lib_mathf
 	], get_output_dir("MasterOfDisguise"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_osa() {
-	prefs := get_prefs(get_source_dir("OSASources", "OSA.psc"), [
-		get_source_dir("psc_deps", "Form.psc"),
-		get_source_dir("SkyuiSDKSources_v5.2", "SKI_ConfigBase.psc"),
-		get_source_dir("PapyrusUtilSources", "PapyrusUtil.psc")
+	prefs := get_prefs(osa, [
+		skyrim_deps,
+		skyui_sdk_52,
+		papyrus_util
 	], get_output_dir("OSA"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_ostim() {
-	prefs := get_prefs(get_source_dir("OStimNGSources", "OStimAddon.psc"), [
-		get_source_dir("psc_deps", "Form.psc"),
-		get_source_dir("UIExtensionsSources", "UIExtensions.psc"),
-		get_source_dir("PapyrusUtilSources", "PapyrusUtil.psc"),
-		get_source_dir("NiOverrideSources", "NiOverride.psc"),
-		get_source_dir("JContainersSources", "JContainers.psc"),
-		get_source_dir("SkyuiSDKSources_v5.1", "SKI_ConfigBase.psc"),
-		get_source_dir("ConsoleUtilSources", "ConsoleUtil.psc")
+	prefs := get_prefs(ostim, [
+		skyrim_deps,
+		ui_extensions,
+		papyrus_util,
+		ni_override,
+		jcontainers,
+		skyui_sdk_51,
+		console_util
 	], get_output_dir("OStim"))
 
 	builder.compile(&prefs)
 }
 
 fn test_project_sexlab(){
-	prefs := get_prefs(get_source_dir("SexLabSources", "SexLabFramework.psc"), [
-		get_source_dir("psc_deps", "Form.psc"),
-		get_source_dir("NiOverrideSources", "NiOverride.psc"),
-		get_source_dir("MfgFixSources", "MfgFix_Settings.psc"),
-		get_source_dir("SkyuiSDKSources_v5.1", "SKI_ConfigBase.psc"),
-		get_source_dir("LovenseAPISources", "Lovense.psc"),
-		get_source_dir("FNISSources", "fnis.psc"),
-		get_source_dir("PapyrusUtilSources", "PapyrusUtil.psc")
+	prefs := get_prefs(sexlab, [
+		skyrim_deps,
+		ni_override,
+		mfgfix,
+		skyui_sdk_51,
+		lovense_api,
+		fnis,
+		papyrus_util
 	], get_output_dir("SexLab"))
 
 	builder.compile(&prefs)
