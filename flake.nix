bcb4c005257293fa0e43f134987d36900770f132
DioKyrie <DioKyrie@proton.me>
2026-03-16 13:09:52 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "A flake for papyrus-compiler";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          config.allowUnfree = true;
+        };
+        tag = "2026.03.15"; # Use tags to avoid hash mismatch after each commit to master.
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "papyrus-compiler";
+          version = tag;
+          # Local source code (no external files needed)
+          src = pkgs.fetchFromGitHub {
+            owner = "russo-2025";
+            repo = "papyrus-compiler";
+            rev = tag;
+            hash = "sha256-0zWJdifY7XB8XbFP2ZPqiu5MKSHjcyFXXdadO/CWqew=";
+            fetchSubmodules = true;
+            leaveDotGit = true; # Preserve .git/ upon unpacking to satisfy @VMODHASH
+          };
+          buildPhase = ''
+            #This will set home to /tmp directory rather than /homeless-shelter/
+            export HOME=$TMP
+            # Build
+            v -prod -g -gc none -o "bin/papyrus" compiler.v
+          '';
+          installPhase = ''
+            mkdir -p $out/bin/
+            cp ./bin/papyrus $out/bin/
+          '';
+          nativeBuildInputs = with pkgs; [
+            vlang
+          ];
+        };
+        # Development shell with tools for hacking on the package
+        # Enter with `nix develop`
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          shellHook = ''
+            export PS1="$PS1[nix develop]:"
+            echo "Welcome to the dev shell."
+            echo "Build the package with: nix build"
+          '';
+        };
+      });
+}
