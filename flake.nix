5fe746e76170466a2d27c7b068d22425ec9ee36c
DioKyrie <DioKyrie@proton.me>
2026-03-16 17:03:17 +0200

    Add comments to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "A flake for papyrus-compiler";
+  description = "A flake to build papyrus-compiler package for nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Get access to Nix packages.
+    flake-utils.url = "github:numtide/flake-utils"; # Support package build on differen architectures.
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -12,20 +12,20 @@
           system = system;
           config.allowUnfree = true;
         };
-        tag = "2026.03.15"; # Use tags to avoid hash mismatch after each commit to master.
+        tag = "2026.03.15"; # Use tags of stable releases, instead of using master branch as src, to avoid hash mismatch, which will change on each commit to master.
       in
       {
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "papyrus-compiler";
+        packages.default = pkgs.stdenv.mkDerivation { # packages.default is used for build and install. E.g. nix build .# is used for default; nix build .#abc is used for packages.abc.
+          pname = "papyrus-compiler"; # Package name.
           version = tag;
-          # Local source code (no external files needed)
+          # Source code from GitHub.
           src = pkgs.fetchFromGitHub {
             owner = "russo-2025";
             repo = "papyrus-compiler";
             rev = tag;
-            hash = "sha256-0zWJdifY7XB8XbFP2ZPqiu5MKSHjcyFXXdadO/CWqew=";
+            hash = "sha256-0zWJdifY7XB8XbFP2ZPqiu5MKSHjcyFXXdadO/CWqew="; # Hash, if the tag was changed to new, this line will cause error, we will need to replace this hash. New hash will be in the error itself.
+            # E.g. Expected - Old Hash, Got - New Hash. Use whatever hash is displayed inside of the Got message.
             fetchSubmodules = true;
-            #leaveDotGit = true; # Preserve .git/ upon unpacking to satisfy @VMODHASH
           };
           buildPhase = ''
             # This will set home to /tmp directory rather than /homeless-shelter/
@@ -33,30 +33,32 @@
             # so we add this export.
             export HOME=$TMP
 
-            # Extract the actual commit hash and patch @VMODHASH in sys_info.v
+            # Extract the actual commit hash and patch @VMODHASH in sys_info.v. Required as Nix build occurs in isolated environment.
             echo "Patching sys_info.v with commit hash @VMODHASH"
             sed -i "s/@VMODHASH/\"$(git rev-parse HEAD)\"/" modules/papyrus/util/sys_info.v
 
-            # Build papyrus-compiler
+            # Build papyrus-compiler.
             v -prod -g -gc none -o "bin/papyrus-compiler" compiler.v
           '';
           installPhase = ''
+            # Make a package folder in Nix Store, and place our build output there.
             mkdir -p $out/bin/
             cp ./bin/papyrus-compiler $out/bin/
           '';
           nativeBuildInputs = with pkgs; [
+            # All build dependencies should be listed here.
             vlang
             git
           ];
         };
-        # Development shell with tools for hacking on the package
-        # Enter with `nix develop`
+        # Development shell with tools for manual building and tesing of the package.
+        # Enter with `nix develop` command. Must be inside of the folder where flake.nix is located.
         devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.default ];
+          inputsFrom = [ self.packages.${system}.default ]; # Use same packages that listed in build dependencies.
           shellHook = ''
-            export PS1="$PS1[nix develop]:"
+            export PS1="$PS1[nix develop]:" # Add a postfix, so we see that we are inside of a nix develop shell.
             echo "Welcome to the dev shell."
-            echo "Build the package with: nix build"
+            echo "Build the package with: nix build .#"
             echo "Use built package with: ./result/bin/papyrus-compiler"
           '';
         };
@@ -64,7 +66,7 @@
 }
 
 /*
-Bellow we can see an vague example of how would install of papyrus-compiler look like. Please note that this is only a reference and not complete working flake.
+Bellow we can see a vague example of how one would install papyrus-compiler in NixOS with flakes. Please note that this is only a reference and not complete working flake.
 
 {
   description = "Example papyrus-compiler flake.nix";
