ada60ab8f4864ea855aae1d6b0f7e3abef86071e
DioKyrie <DioKyrie@proton.me>
2026-03-16 15:30:33 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         tag = "2026.03.15"; # Use tags to avoid hash mismatch after each commit to master.
       in
       {
-        packages.papyrus-compiler = pkgs.stdenv.mkDerivation {
+        packages.default = pkgs.stdenv.mkDerivation {
           pname = "papyrus-compiler";
           version = tag;
           # Local source code (no external files needed)
@@ -28,19 +28,21 @@
             #leaveDotGit = true; # Preserve .git/ upon unpacking to satisfy @VMODHASH
           };
           buildPhase = ''
-            #This will set home to /tmp directory rather than /homeless-shelter/
+            # This will set home to /tmp directory rather than /homeless-shelter/
+            # vlang seems to have issues with automating it
+            # so we add this export.
             export HOME=$TMP
 
             # Extract the actual commit hash and patch @VMODHASH in sys_info.v
             echo "Patching sys_info.v with commit hash @VMODHASH"
             sed -i "s/@VMODHASH/\"$(git rev-parse HEAD)\"/" modules/papyrus/util/sys_info.v
 
-            # Build
-            v -prod -g -gc none -o "bin/papyrus" compiler.v
+            # Build papyrus-compiler
+            v -prod -g -gc none -o "bin/papyrus-compiler" compiler.v
           '';
           installPhase = ''
             mkdir -p $out/bin/
-            cp ./bin/papyrus $out/bin/
+            cp ./bin/papyrus-compiler $out/bin/
           '';
           nativeBuildInputs = with pkgs; [
             vlang
@@ -50,11 +52,12 @@
         # Development shell with tools for hacking on the package
         # Enter with `nix develop`
         devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.papyrus-compiler ];
+          inputsFrom = [ self.packages.${system}.default ];
           shellHook = ''
             export PS1="$PS1[nix develop]:"
             echo "Welcome to the dev shell."
-            echo "Build the package with: nix build .#papyrus-compiler"
+            echo "Build the package with: nix build"
+            echo "Use built package ./result/bin/papyrus-compiler"
           '';
         };
       });
